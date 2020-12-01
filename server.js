0b840563165b245db0f1590d52ed7c77e42d68ad
Anox3D <71902913+Anox3D@users.noreply.github.com>
2020-12-01 22:39:14 +0100

    Changed PORT

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ io.on('connection', socket => {
     });
 })
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 2000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
